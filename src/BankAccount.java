cfc9a4b517f4c59c624b198b0c0cd3eeaef6e789
tmuldoon21 <42975519+tmuldoon21@users.noreply.github.com>
2019-05-20 11:28:56 -0400

    Add files via upload

---
+++ src/BankAccount.java
@@ -0,0 +1,31 @@
+
+public class BankAccount 
+{
+	private String name;
+	private int balance;
+	
+	public BankAccount (String n, int b)
+	name = n;
+	balance = b;
+	
+	public BankAccount()
+	{
+		name = "Tyler";
+		balance = 0;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getbalance()
+	{
+		return balance;
+	}
+	
+	public int deposit( )
+	{
+		deposit = balance - 
+	}
+}
